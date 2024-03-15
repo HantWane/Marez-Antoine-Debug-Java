b97a9e5f174d1fab4fddc133e3262929b2b9134d
HantWane <antoine.marez@gmail.com>
2024-03-15 09:19:42 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+/**
+ * Interface for writing symptoms.
+ */
+public interface ISymptomWriter {
+    /**
+     * Writes symptoms to a data destination.
+     * @param symptoms A map containing symptoms and their counts.
+     */
+    void writeSymptoms(Map<String, Integer> symptoms);
+}
